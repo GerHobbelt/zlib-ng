45d1957f16ea085d236a14b89a1e24f3f47f211e
Marcelo Sercheli <msercheli@gmail.com>
2020-11-18 19:41:25 -0500

    Add rez files

---
+++ package.py
@@ -0,0 +1,22 @@
+name = "zlib"
+
+version = "1.2.11"
+
+authors = [
+    "madler"
+]
+
+description = \
+    """
+    Compression library
+    """
+
+requires = [
+    "cmake",
+    "gcc",
+]
+
+build_system = "cmake"
+
+def commands():
+    pass
