c59c599701944bdc5bfe8f3597908c82620001fe
Marcelo Sercheli <msercheli@gmail.com>
2020-11-23 17:10:50 -0500

    Add package REZ file

---
+++ package.py
@@ -0,0 +1,15 @@
+name = "zlib"
+
+version = "1.2.11.1"
+
+authors = ["madler"]
+
+requires = [
+    "cmake",
+    "gcc",
+]
+
+build_system = "cmake"
+
+def commands():
+    env.ZLIB_INCLUDE_DIRS = "{root}"
